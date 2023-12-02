8155f8b73b6ae91ec2fbd49e59333f7c5054b0cc
smu <smu@z107.de>
2023-12-02 11:41:45 +0100

    part 1 day 2

---
+++ day02/2.py
@@ -0,0 +1,38 @@
+
+
+# The Elf would first like to know which games would have been possible if the
+# bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+possible = {'red': 12, 'green': 13, 'blue': 14}
+
+possible_groups = []
+
+
+def check_subset(data, maxsolution):
+    data = data.strip(' ')
+    for cubes in data.split(','):
+        cubes = cubes.strip(' ')
+        tmp = cubes.split(' ')
+        count = int(tmp[0])
+        color = tmp[1]
+        if count > maxsolution[color]:
+            return(False)
+    return(True)
+
+#with open('day02/input_example') as f:
+with open('day02/input') as f:
+    for line in f.readlines():
+        line = line.rstrip()
+        print(line)
+        tmp = line.split(':')
+        game = tmp[0].split(' ')[1]
+        data = tmp[1].split(';')
+        game_possible = True
+        for subset in data:
+            if not check_subset(subset, possible):
+                game_possible = False
+        if game_possible:
+            possible_groups.append(int(game))
+
+
+print(possible_groups)
+print(sum(possible_groups))
