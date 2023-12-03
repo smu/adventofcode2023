9a5691d59f30840393ea33cf5ea68336c13b5940
smu <smu@z107.de>
2023-12-03 12:01:02 +0100

    day 03 part a

---
+++ day03/3.py
@@ -0,0 +1,105 @@
+
+# idea:
+#  1. read symbols and their position
+#  2. find numbers and ther positions_s_
+#  3. compare
+
+
+class Number(object):
+    def __init__(self):
+        self.v = None
+        self.c = []
+
+    def add_match(self, match, x):
+        self.v = int(match.group(0))
+        c0 = match.span()
+        # all coords for all digits
+        for n in range(c0[0], c0[1]):
+            self.c.append((n, y))
+
+    def __str__(self):
+        return(f"{ self.v } ::  { self.c }")
+
+
+class Symbol:
+    def __init__(self):
+        self.s = None
+        self.c = []
+
+    def add_match(self, match, x):
+        self.s = match.group(0)
+        c0 = match.span()
+        self.c.append((c0[0], y))
+
+    def __str__(self):
+        return(f"{ self.s } ::  { self.c }")
+
+    def find_neightboor(self, numbers):
+        neighboors = []
+        for n in numbers:
+            # print(f'Testing { n }')
+            if self.is_neightboor(n):
+                neighboors.append(n.v)
+        return(neighboors)
+
+    def is_neightboor(self, n):
+        c = self.c[0]
+        cx = c[0]
+        cy = c[1]
+        nc = n.c
+        for y in (-1,0,1):
+            for x in (-1,0,1):
+                if (cx+x,cy+y) in nc:
+                    print(n)
+                    return(True)
+        return(False)
+
+                
+
+
+import re
+
+# symbol_re = re.compile('^[\w\.]{1}')
+symbol_re = re.compile('[=+!@#$%^&*()/-]{1}')
+number_re = re.compile('\d+')
+
+numbers  = []
+symbols =[]
+
+#with open('day03/input_example') as f:
+with open('day03/input') as f:
+    y = 0
+    for line in f.readlines():
+        line = line.rstrip()
+        # print(line)
+        for s in symbol_re.finditer(line):
+            # print(s)
+            nn = Symbol()
+            nn.add_match(s, y)
+            symbols.append(nn)
+        for n in number_re.finditer(line):
+            nn = Number()
+            nn.add_match(n, y)
+            numbers.append(nn)
+        y += 1
+
+# import sys
+# sys.exit(0)
+
+# print(len(numbers))
+# for n in numbers:
+    # print(n)
+
+# print(len(symbols))
+
+part_numbers = []
+for s in symbols:
+    print(s)
+    # find neighbooring numbers.
+    for num in s.find_neightboor(numbers):
+        part_numbers.append(num)
+
+
+print('-----------------')
+print(part_numbers)
+print(sum(part_numbers))
